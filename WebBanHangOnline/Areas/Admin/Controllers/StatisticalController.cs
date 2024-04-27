dae0edc8397bf1cef53c92dc4b75e0be7c10c0be
ngocrobert <nguyenxuanngoc18012001@gmail.com>
2024-04-27 19:05:45 +0700

    statistical function (doanh thu - loi nhuan)

---
+++ WebBanHangOnline/Areas/Admin/Controllers/StatisticalController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebBanHangOnline.Data;
+
+namespace WebBanHangOnline.Areas.Admin.Controllers
+{
+    [Area("admin")]
+    [Route("admin/statistical")]
+    public class StatisticalController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public StatisticalController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        [Route("getStatistical")]
+        public IActionResult GetStatistical(string fromDate, string toDate)
+        {
+            var query = from o in _db.Orders
+                        join od in _db.OrderDetails
+                        on o.Id equals od.OrderId
+                        join p in _db.Products
+                        on od.ProductId equals p.Id
+                        select new
+                        {
+                            CreatedDate = o.CreatedDate,
+                            Quantity = od.Quantity,
+                            Price = od.Price,
+                            OriginalPrice = p.OriginalPrice
+                        };
+            if(!string.IsNullOrEmpty(fromDate))
+            {
+                DateTime startDate = DateTime.ParseExact(fromDate, "dd/MM/yyyy", null);
+                query = query.Where(x => x.CreatedDate >= startDate);
+            }
+            if (!string.IsNullOrEmpty(toDate))
+            {
+                DateTime endDate = DateTime.ParseExact(toDate, "dd/MM/yyyy", null);
+                query = query.Where(x => x.CreatedDate < endDate);
+            }
+            var result = query.GroupBy(x => x.CreatedDate.Date).Select(x => new
+            {
+                Date = x.Key,
+                TotalBuy = x.Sum(y=>y.Quantity * y.OriginalPrice),
+                TotalSell = x.Sum(y=>y.Quantity * y.Price),
+            }).Select(x => new
+            {
+                Date = x.Date,
+                DoanhThu = x.TotalSell,
+                LoiNhuan = x.TotalSell - x.TotalBuy
+            }); 
+            return Json(new {data = result});
+        }
+    }
+    
+}
---
+++ WebBanHangOnline/Data/Migrations/20240427074814_UpdateProductOriginalPrice.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebBanHangOnline.Data.Migrations
+{
+    public partial class UpdateProductOriginalPrice : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "OriginalPrice",
+                table: "tb_Product",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "OriginalPrice",
+                table: "tb_Product");
+        }
+    }
+}
